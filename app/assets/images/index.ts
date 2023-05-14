38f8bd1f10e81cadba039557fb58420630e1368a
balogunofafrica <balogunakanbi.k@gmail.com>
2023-05-14 20:27:31 +0100

    chore: add image and icon assets

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,1 +1,6 @@
-export { default as sky } from "./sky.png";
+export { default as dollar } from "./dollar.png";
+export { default as done } from "./done.png";
+export { default as gift } from "./gift.png";
+export { default as gradientBg } from "./gradient-bg.png";
+export { default as plan } from "./plan.png";
+export { default as time } from "./time.png";
--- app/components/icon/icon-pack.ts
+++ app/components/icon/icon-pack.ts
@@ -1,11 +1,35 @@
-import chevronLeft from "@/assets/svg/chevron-left.svg";
+import backArrow from "@/assets/svg/back-arrow.svg";
+import bell from "@/assets/svg/bell.svg";
+import calendar from "@/assets/svg/calendar.svg";
+import cancel from "@/assets/svg/cancel.svg";
+import check from "@/assets/svg/check.svg";
+import chevronRight from "@/assets/svg/chevron-right.svg";
+import deleteIcon from "@/assets/svg/delete.svg";
 import eye from "@/assets/svg/eye.svg";
 import eyeOff from "@/assets/svg/eye-off.svg";
+import gear from "@/assets/svg/gear.svg";
+import plus from "@/assets/svg/plus.svg";
+import question from "@/assets/svg/question.svg";
+import rise from "@/assets/svg/rise.svg";
+import share from "@/assets/svg/share.svg";
+import upArrow from "@/assets/svg/up-arrow.svg";
 
 export const IconPack = {
-  "chevron-left-icon": chevronLeft,
-  "eye-icon": eye,
-  "eye-off-icon": eyeOff,
+  "back-arrow": backArrow,
+  bell,
+  calendar,
+  cancel,
+  check,
+  "chevron-right": chevronRight,
+  delete: deleteIcon,
+  eye,
+  "eye-off": eyeOff,
+  gear,
+  plus,
+  question,
+  rise,
+  share,
+  "up-arrow": upArrow,
 };
 
 export type IconName = keyof typeof IconPack;
