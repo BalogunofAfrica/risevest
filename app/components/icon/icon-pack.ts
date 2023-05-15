54ecc01f6c4c7c2e5fa1997a19af004ac148bc10
balogunofafrica <balogunakanbi.k@gmail.com>
2023-05-15 19:57:23 +0100

    chore: add tab icons

--- app/components/icon/icon-pack.ts
+++ app/components/icon/icon-pack.ts
@@ -1,3 +1,5 @@
+import accountActive from "@/assets/svg/account-active.svg";
+import accountInactive from "@/assets/svg/account-inactive.svg";
 import backArrow from "@/assets/svg/back-arrow.svg";
 import bank from "@/assets/svg/bank.svg";
 import bell from "@/assets/svg/bell.svg";
@@ -11,18 +13,28 @@ import deleteIcon from "@/assets/svg/delete.svg";
 import ellipses from "@/assets/svg/ellipses.svg";
 import eye from "@/assets/svg/eye.svg";
 import eyeOff from "@/assets/svg/eye-off.svg";
+import feedActive from "@/assets/svg/feed-active.svg";
+import feedInactive from "@/assets/svg/feed-inactive.svg";
 import forwardArrow from "@/assets/svg/forward-arrow.svg";
 import gear from "@/assets/svg/gear.svg";
+import homeActive from "@/assets/svg/home-active.svg";
+import homeInactive from "@/assets/svg/home-inactive.svg";
 import ngnBank from "@/assets/svg/ngn-bank.svg";
 import notice from "@/assets/svg/notice.svg";
+import plansActive from "@/assets/svg/plans-active.svg";
+import plansInactive from "@/assets/svg/plans-inactive.svg";
 import plus from "@/assets/svg/plus.svg";
 import question from "@/assets/svg/question.svg";
 import rise from "@/assets/svg/rise.svg";
 import share from "@/assets/svg/share.svg";
 import upArrow from "@/assets/svg/up-arrow.svg";
 import usdCard from "@/assets/svg/usd-card.svg";
+import walletActive from "@/assets/svg/wallet-active.svg";
+import walletInactive from "@/assets/svg/wallet-inactive.svg";
 
 export const IconPack = {
+  "account-active": accountActive,
+  "account-inactive": accountInactive,
   "back-arrow": backArrow,
   bank,
   bell,
@@ -36,16 +48,24 @@ export const IconPack = {
   ellipses,
   eye,
   "eye-off": eyeOff,
+  "feed-active": feedActive,
+  "feed-inactive": feedInactive,
   "forward-arrow": forwardArrow,
   gear,
+  "home-active": homeActive,
+  "home-inactive": homeInactive,
   "ngn-bank": ngnBank,
   notice,
+  "plans-active": plansActive,
+  "plans-inactive": plansInactive,
   plus,
   question,
   rise,
   share,
   "up-arrow": upArrow,
   "usd-card": usdCard,
+  "wallet-active": walletActive,
+  "wallet-inactive": walletInactive,
 };
 
 export type IconName = keyof typeof IconPack;
