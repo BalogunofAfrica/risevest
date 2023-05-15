ff85435e93eda4e61000f561c3c8cbcad464f21c
balogunofafrica <balogunakanbi.k@gmail.com>
2023-05-15 23:15:50 +0100

    refactor: remove dead code

--- app/hooks/system/index.ts
+++ app/hooks/system/index.ts
@@ -1,3 +1,2 @@
 export * from "./use-app-start";
 export * from "./use-app-state";
-export * from "./use-dark-mode";
--- app/hooks/system/use-dark-mode.ts
+++
@@ -1,7 +0,0 @@
-import { ColorSchemeName, useColorScheme } from "react-native";
-
-export function useDarkMode() {
-  const systemScheme = useColorScheme() as NonNullable<ColorSchemeName>;
-
-  return systemScheme === "dark";
-}
