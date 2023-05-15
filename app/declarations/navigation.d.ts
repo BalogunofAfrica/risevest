963aa9c075e75d8a3a69dda896f97ab733bf3a3f
balogunofafrica <balogunakanbi.k@gmail.com>
2023-05-15 20:43:14 +0100

    feat(dx): augment route types

--- app/declarations/navigation.d.ts
+++ app/declarations/navigation.d.ts
@@ -1,9 +1,17 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 /* eslint-disable @typescript-eslint/no-empty-interface */
 
+import {
+  HomeNavigationRoutes,
+  TabNavigationRoutes,
+} from "@/navigation/app-nav";
+import { AuthNavigationRoutes } from "@/navigation/auth-nav";
 import { RootNavigationRoutes } from "@/navigation/root-nav";
 
-export type StackParamsList = RootNavigationRoutes;
+export type StackParamsList = RootNavigationRoutes &
+  AuthNavigationRoutes &
+  HomeNavigationRoutes &
+  TabNavigationRoutes;
 
 declare global {
   namespace ReactNavigation {
